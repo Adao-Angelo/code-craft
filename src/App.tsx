3742d86b0cc182bb0b14921015c9afc67f66bf3d
adao-angelo <adaobegginer@gmail.com>
2026-07-07 16:27:21 +0100

    feat update date of start

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,32 @@
-import { useGSAP } from "@gsap/react";
+import { useGSAP } from '@gsap/react';
 import {
   ArrowUpRight03Icon,
   CustomerSupportIcon,
-} from "@hugeicons/core-free-icons";
-import { HugeiconsIcon } from "@hugeicons/react";
-import gsap from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
+} from '@hugeicons/core-free-icons';
+import { HugeiconsIcon } from '@hugeicons/react';
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
-import { useEffect, useRef } from "react";
+import { useEffect, useRef } from 'react';
 
-import Lenis from "lenis";
+import Lenis from 'lenis';
 
-import AOS from "aos";
-import "aos/dist/aos.css";
-import SplitType from "split-type";
-import "./App.scss";
-import BannerTechnologies from "./components/banner-technologies/banner-technologies";
-import CourseJourney from "./components/course-journey/section";
-import EnrollmentForm from "./components/enrollment-form/enrollment-form";
-import { EnrollmentSection } from "./components/enrollment-section/enrollment-section";
-import { Image } from "./components/Image/image";
-import Logo from "./components/Logo";
-import { TechCard } from "./components/tech-card/tech-card";
-import { Button } from "./components/ui/Button/Button";
-import GridPattern from "./components/ui/GridPattern/GridPattern";
-import Header from "./components/ui/Header";
-import Timer from "./components/ui/Timer/timer";
-import { techs } from "./constants/techs";
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import SplitType from 'split-type';
+import './App.scss';
+import BannerTechnologies from './components/banner-technologies/banner-technologies';
+import CourseJourney from './components/course-journey/section';
+import EnrollmentForm from './components/enrollment-form/enrollment-form';
+import { EnrollmentSection } from './components/enrollment-section/enrollment-section';
+import { Image } from './components/Image/image';
+import Logo from './components/Logo';
+import { TechCard } from './components/tech-card/tech-card';
+import { Button } from './components/ui/Button/Button';
+import GridPattern from './components/ui/GridPattern/GridPattern';
+import Header from './components/ui/Header';
+import Timer from './components/ui/Timer/timer';
+import { techs } from './constants/techs';
 gsap.registerPlugin(ScrollTrigger);
 
 function App() {
@@ -40,12 +40,12 @@ function App() {
 
   useGSAP(
     () => {
-      gsap.from(".gsap-hero__box", {
+      gsap.from('.gsap-hero__box', {
         y: 80,
         opacity: 0,
         duration: 1,
       });
-      gsap.from(".gsap-hero__frontend", {
+      gsap.from('.gsap-hero__frontend', {
         y: 80,
         x: 300,
         opacity: 0,
@@ -53,13 +53,13 @@ function App() {
         rotate: 50,
       });
 
-      gsap.from(".gsap-hero__image", {
+      gsap.from('.gsap-hero__image', {
         y: 80,
         opacity: 0,
         duration: 1,
       });
 
-      gsap.from(".gsap-hero__title", {
+      gsap.from('.gsap-hero__title', {
         y: 40,
         opacity: 0,
         duration: 0.8,
@@ -67,20 +67,20 @@ function App() {
         delay: 0.2,
       });
     },
-    { scope: scopeRef },
+    { scope: scopeRef }
   );
 
   useGSAP(
     () => {
-      const split = new SplitType(".gsap-content__text", {
-        types: "chars,lines",
+      const split = new SplitType('.gsap-content__text', {
+        types: 'chars,lines',
       });
 
       const track = document.querySelector(
-        ".gsap-tech-track",
+        '.gsap-tech-track'
       ) as HTMLDivElement;
 
-      const firstCard = document.querySelector(".tech-card") as HTMLElement;
+      const firstCard = document.querySelector('.tech-card') as HTMLElement;
 
       const centerOffset = window.innerWidth / 2 - firstCard.offsetWidth / 2;
       gsap.set(track, {
@@ -88,9 +88,9 @@ function App() {
       });
       const timeline = gsap.timeline({
         scrollTrigger: {
-          trigger: ".gsap-full-section",
-          start: "top top",
-          end: () => "+=" + window.innerHeight * 2,
+          trigger: '.gsap-full-section',
+          start: 'top top',
+          end: () => '+=' + window.innerHeight * 2,
           pin: true,
           scrub: true,
         },
@@ -102,7 +102,7 @@ function App() {
         direction: 1,
       });
 
-      timeline.to(".gsap-content__text", {
+      timeline.to('.gsap-content__text', {
         opacity: 0,
         y: -50,
         duration: 1,
@@ -112,12 +112,12 @@ function App() {
         y: 120,
         opacity: 0,
         duration: 1,
-        ease: "power3.out",
+        ease: 'power3.out',
       });
 
       timeline.to(track, {
         x: centerOffset - (track.scrollWidth - firstCard.offsetWidth),
-        ease: "none",
+        ease: 'none',
         duration: 4,
       });
 
@@ -128,61 +128,61 @@ function App() {
         split.revert();
       };
     },
-    { scope: scopeRef },
+    { scope: scopeRef }
   );
 
   useGSAP(
     () => {
-      gsap.set(".mask-image-container", {
-        maskSize: "90vw",
-        webkitMaskSize: "90vw",
+      gsap.set('.mask-image-container', {
+        maskSize: '90vw',
+        webkitMaskSize: '90vw',
       });
 
-      gsap.set(".greenScreen", {
+      gsap.set('.greenScreen', {
         opacity: 0,
       });
 
-      gsap.set(".logo-on-mask", {
+      gsap.set('.logo-on-mask', {
         x: 0,
         rotation: 0,
       });
 
-      gsap.set(".title-on-mask", {
+      gsap.set('.title-on-mask', {
         opacity: 0,
         x: 120,
       });
 
       const tl = gsap.timeline({
         scrollTrigger: {
-          trigger: ".mask-container",
-          start: "top -25%+=200",
-          end: "+=200%",
+          trigger: '.mask-container',
+          start: 'top -25%+=200',
+          end: '+=200%',
           scrub: 1,
-          pin: ".mask-image-container",
+          pin: '.mask-image-container',
           anticipatePin: 1,
         },
       });
 
-      tl.to(".mask-image-container", {
-        maskSize: "12vw",
-        webkitMaskSize: "12vw",
-        ease: "none",
+      tl.to('.mask-image-container', {
+        maskSize: '12vw',
+        webkitMaskSize: '12vw',
+        ease: 'none',
       })
-        .to(".greenScreen", {
+        .to('.greenScreen', {
           opacity: 1,
-          ease: "none",
+          ease: 'none',
         })
-        .to(".mask-image-container", {
+        .to('.mask-image-container', {
           opacity: 0,
           duration: 0.8,
         });
-      tl.to(".logo-on-mask", {
+      tl.to('.logo-on-mask', {
         y: 70,
         rotation: 360,
         duration: 1.2,
-        ease: "power3.inOut",
+        ease: 'power3.inOut',
       }).fromTo(
-        ".title-on-mask",
+        '.title-on-mask',
         {
           x: 80,
           opacity: 0,
@@ -191,12 +191,12 @@ function App() {
           x: 0,
           opacity: 1,
           duration: 1.2,
-          ease: "power3.out",
+          ease: 'power3.out',
         },
-        "<",
+        '<'
       );
     },
-    { scope: scopeRef },
+    { scope: scopeRef }
   );
 
   useEffect(() => {
@@ -208,7 +208,7 @@ function App() {
       lenis.raf(time * 1000);
     };
 
-    lenis.on("scroll", ScrollTrigger.update);
+    lenis.on('scroll', ScrollTrigger.update);
 
     gsap.ticker.add(ticker);
     gsap.ticker.lagSmoothing(0);
@@ -223,38 +223,38 @@ function App() {
 
   return (
     <>
-      <div className="page-content" ref={scopeRef}>
+      <div className='page-content' ref={scopeRef}>
         <div>
-          <section id="hero">
+          <section id='hero'>
             <Header />
-            <div className="container">
-              <div className="hero-date-session">
+            <div className='container'>
+              <div className='hero-date-session'>
                 <span>A primeira sessão começa no dia 10 de Julho, 2026</span>
-                <Image src="/logo.svg" alt="logo"></Image>
+                <Image src='/logo.svg' alt='logo'></Image>
               </div>
             </div>
 
-            <div className="hero-content-container">
-              <div className="container">
-                <div className="hero-content gsap-hero__box">
-                  <h1 className="masterclass hero-line gsap-hero__title">
+            <div className='hero-content-container'>
+              <div className='container'>
+                <div className='hero-content gsap-hero__box'>
+                  <h1 className='masterclass hero-line gsap-hero__title'>
                     MASTERCLASS
                   </h1>
-                  <h1 className="frontend hero-line gsap-hero__frontend">
+                  <h1 className='frontend hero-line gsap-hero__frontend'>
                     FRONTEND
                   </h1>
-                  <h1 className="development hero-line gsap-hero__title">
+                  <h1 className='development hero-line gsap-hero__title'>
                     DEVELOPMENT
                   </h1>
                 </div>
-                <div className="call-actions">
-                  <a href="#content-section">
+                <div className='call-actions'>
+                  <a href='#content-section'>
                     <Button
                       leftElement={
                         <HugeiconsIcon
                           icon={ArrowUpRight03Icon}
                           size={24}
-                          color="currentColor"
+                          color='currentColor'
                           strokeWidth={1.5}
                         />
                       }
@@ -262,14 +262,14 @@ function App() {
                       Explorar conteúdo
                     </Button>
                   </a>
-                  <a href="#price-section">
-                    <Button variant="secondary">Garanta sua vaga</Button>
+                  <a href='#price-section'>
+                    <Button variant='secondary'>Garanta sua vaga</Button>
                   </a>
                 </div>
 
-                <div className="content-meet-and-timer">
-                  <div className="google-meet-section">
-                    <Image src="/google-meet-logo.png"></Image>
+                <div className='content-meet-and-timer'>
+                  <div className='google-meet-section'>
+                    <Image src='/google-meet-logo.png'></Image>
                     <span>GOOGLE MEET</span>
                   </div>
 
@@ -277,30 +277,30 @@ function App() {
                 </div>
               </div>
             </div>
-            <div className="image-and-illustrations">
-              <Image className="logo-image" src="/logo.svg"></Image>
+            <div className='image-and-illustrations'>
+              <Image className='logo-image' src='/logo.svg'></Image>
               <Image
-                className="hero-girl-image gsap-hero__image"
-                src="/hero-girl.png"
+                className='hero-girl-image gsap-hero__image'
+                src='/hero-girl.png'
               ></Image>
 
-              <div className="price-card">
-                <div className="content-frontend-text">
-                  <Image src="/logo.svg"></Image>
+              <div className='price-card'>
+                <div className='content-frontend-text'>
+                  <Image src='/logo.svg'></Image>
                   <p>DEV FRONTEND</p>
                 </div>
-                <p className="price">12.000kz</p>
+                <p className='price'>12.000kz</p>
               </div>
             </div>
           </section>
           <BannerTechnologies />
-          <section id="content-section" className="gsap-full-section">
-            <div className="gsap-content">
-              <div className="container">
-                <h2 className="section-title gsap-content__title">
+          <section id='content-section' className='gsap-full-section'>
+            <div className='gsap-content'>
+              <div className='container'>
+                <h2 className='section-title gsap-content__title'>
                   O QUE IRÁ APRENDER
                 </h2>
-                <p className="section-description  gsap-content__text">
+                <p className='section-description  gsap-content__text'>
                   Aprenda através da prática, construindo projetos reais
                   enquanto desenvolve competências em HTML, CSS, JavaScript,
                   design responsivo, ferramentas modernas e boas práticas de
@@ -309,7 +309,7 @@ function App() {
               </div>
             </div>
 
-            <div className="gsap-tech-track">
+            <div className='gsap-tech-track'>
               {techs.map((tech, index) => (
                 <TechCard
                   key={index}
@@ -319,7 +319,7 @@ function App() {
                     <HugeiconsIcon
                       icon={ArrowUpRight03Icon}
                       size={20}
-                      color="currentColor"
+                      color='currentColor'
                     />
                   }
                 />
@@ -330,16 +330,16 @@ function App() {
 
         <CourseJourney />
 
-        <section className="mask-section">
-          <div className="mask-container #Container">
-            <div className="mask-image-container #Hero">
-              <Image src="/mask.png" className="mask-image" />
+        <section className='mask-section'>
+          <div className='mask-container #Container'>
+            <div className='mask-image-container #Hero'>
+              <Image src='/mask.png' className='mask-image' />
 
-              <div className="greenScreen"></div>
+              <div className='greenScreen'></div>
             </div>
-            <div className="content-mask">
-              <Image src="/logo.svg" className="logo-on-mask" />
-              <h1 className="title-on-mask">Code Craft</h1>
+            <div className='content-mask'>
+              <Image src='/logo.svg' className='logo-on-mask' />
+              <h1 className='title-on-mask'>Code Craft</h1>
             </div>
           </div>
         </section>
@@ -347,10 +347,10 @@ function App() {
         <EnrollmentForm />
       </div>
 
-      <div className="footer-spacer" />
+      <div className='footer-spacer' />
 
-      <footer className="footer">
-        <div className="footer-grid-bg">
+      <footer className='footer'>
+        <div className='footer-grid-bg'>
           <GridPattern
             width={40}
             height={40}
@@ -362,33 +362,33 @@ function App() {
           />
         </div>
 
-        <div className="footer-content">
-          <div className="content-timer">
-            <p className="timer-title"> As aulas começam em:</p>
+        <div className='footer-content'>
+          <div className='content-timer'>
+            <p className='timer-title'> As aulas começam em:</p>
             <Timer />
           </div>
-          <div className="footer-info">
+          <div className='footer-info'>
             <Logo />
-            <div className="footer-google-meet-section">
-              <Image src="/google-meet-logo.png" />
+            <div className='footer-google-meet-section'>
+              <Image src='/google-meet-logo.png' />
               <span>GOOGLE MEET</span>
             </div>
-            <div className="footer-call-section">
-              <div className="icon">
+            <div className='footer-call-section'>
+              <div className='icon'>
                 <HugeiconsIcon
                   icon={CustomerSupportIcon}
                   size={28}
-                  color="currentColor"
+                  color='currentColor'
                   strokeWidth={2}
                 />
               </div>
               <span>947 636 149</span>
             </div>
-            <Image className="float-box" src="/start.svg" />
+            <Image className='float-box' src='/start.svg' />
           </div>
 
-          <div className="footer-code-area">
-            <Image src="/logo.svg" />
+          <div className='footer-code-area'>
+            <Image src='/logo.svg' />
             <span>Code Craft</span>
           </div>
         </div>
--- src/components/ui/Timer/timer.tsx
+++ src/components/ui/Timer/timer.tsx
@@ -1,28 +1,28 @@
-import { useCountdown } from "../../../hooks/use-countdown";
-import "./timer.scss";
+import { useCountdown } from '../../../hooks/use-countdown';
+import './timer.scss';
 
 export default function Timer() {
-  const { days, hours, minutes, seconds } = useCountdown("2026-07-10T00:00:00");
+  const { days, hours, minutes, seconds } = useCountdown('2026-07-15T00:00:00');
   return (
-    <div className="countdown">
-      <div className="time-box">
-        <span className="time-value">{days}</span>
-        <span className="time-label">Dias</span>
+    <div className='countdown'>
+      <div className='time-box'>
+        <span className='time-value'>{days}</span>
+        <span className='time-label'>Dias</span>
       </div>
 
-      <div className="time-box">
-        <span className="time-value">{hours}</span>
-        <span className="time-label">Horas</span>
+      <div className='time-box'>
+        <span className='time-value'>{hours}</span>
+        <span className='time-label'>Horas</span>
       </div>
 
-      <div className="time-box">
-        <span className="time-value">{minutes}</span>
-        <span className="time-label">Minutos</span>
+      <div className='time-box'>
+        <span className='time-value'>{minutes}</span>
+        <span className='time-label'>Minutos</span>
       </div>
 
-      <div className="time-box">
-        <span className="time-value">{seconds}</span>
-        <span className="time-label">Segundos</span>
+      <div className='time-box'>
+        <span className='time-value'>{seconds}</span>
+        <span className='time-label'>Segundos</span>
       </div>
     </div>
   );
